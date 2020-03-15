d3069e30d4dc8de3819cdbf0370c520289f71f26
Nathdanai Somprasong <tam@Nathdanais-MacBook-Pro.local>
2020-03-14 19:43:28 -0700

    Start adding to git hub

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App/App.jsx'
 
-ReactDOM.render(<div>This is my app</div>, document.getElementById('root'));
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
 
